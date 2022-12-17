6bbf1f1794d0537e46f1fa606434850ce87d507c
hiper25 <2243168976@qq.com>
2022-12-17 18:40:46 +0000

    Spacewar: Update Nothing OS 1.1.7 Blobs
    Signed-off-by: hiper25 <2243168976@qq.com>

--- aosp_Spacewar.mk
+++ aosp_Spacewar.mk
@@ -27,6 +27,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-nothing
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=Spacewar \
-    PRIVATE_BUILD_DESC="Spacewar-user 12 SKQ1.211230.001 1666412462 release-keys"
+    PRIVATE_BUILD_DESC="Spacewar-user 12 SKQ1.211230.001 1669043167 release-keys"
 
-BUILD_FINGERPRINT := Nothing/Spacewar/Spacewar:12/SKQ1.211230.001/1666412462:user/release-keys
+BUILD_FINGERPRINT := Nothing/Spacewar/Spacewar:12/SKQ1.211230.001/1669043167:user/release-keys
